eee6b578ee699e56661299695981c2512fd1fc1a
Aman kohare <amankohare@gmail.com>
2024-08-06 18:19:07 +0530

    adding some thibg

--- components/Header.js
+++ components/Header.js
@@ -5,26 +5,26 @@ import '@fortawesome/fontawesome-free/css/all.css'; // Ensure the path is correc
 
 
 const Header = () => {
-  return (
-    <header className={styles.header}>
-      <div className={styles.logo}>
-        <img src="/vector.png" alt="Appscrip Logo" className={styles.logoImage} />
-      </div>
-      <div className={styles.title}>
-        <h1>APPSCRIP</h1>
-      </div>
-      <div className={styles.icons}>
-        <i className={`fa fa-search  ${styles.icon}`}></i>
-        <i className={`fa-solid fa-bag-shopping  ${styles.icon}`}></i>
-        <i className={`fa-regular fa-heart ${styles.icon}`}></i>
-        <i className={`fa-regular fa-user ${styles.icon}`}></i>
-        <select className={styles.languageSelect}>
-          <option value="en">ENG</option>
-          <option value="hi">HINDI</option>
-        </select>
-      </div>
-    </header>
-  );
+    return (
+        <header className={styles.header}>
+            <div className={styles.icons}>
+                <i className={`fa-solid fa-vector-square  ${styles.icon1}`}></i>
+            </div>
+            <div className={styles.title}>
+                <h1>APPSCRIP</h1>
+            </div>
+            <div className={styles.icons}>
+                <i className={`fa fa-search  ${styles.icon}`}></i>
+                <i className={`fa-solid fa-bag-shopping  ${styles.icon}`}></i>
+                <i className={`fa-regular fa-heart ${styles.icon}`}></i>
+                <i className={`fa-regular fa-user ${styles.icon}`}></i>
+                <select className={styles.languageSelect}>
+                    <option value="en">ENG</option>
+                    <option value="hi">HINDI</option>
+                </select>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
